a105e03e766e00774f3cd060b795a3b6540999b0
stsrjpeyu <146166899+stsrjpeyu@users.noreply.github.com>
2025-04-30 22:01:48 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 
 const bot = new Client({
   intents: [
-    GatewayIntentBits.GuildMessages,
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent, // メッセージ内容を取得するために必要
   ],
 });
 
@@ -15,10 +15,11 @@ const TRIGGER_MESSAGE = process.env.TRIGGER_MESSAGE;
 bot.login(process.env.DISCORD_BOT_TOKEN);
 
 bot.on("ready", () => {
-  console.log("The AI bot is online"); //message when bot is online
+  console.log("The AI bot is online");
 });
 
 bot.on("messageCreate", async (message) => {
+  if (message.author.bot) return; // Bot自身のメッセージを無視
   if (message.content.includes(TRIGGER_MESSAGE)) {
     console.log("🚀 NOOB request: ", message.content);
     const question = message.content.split(TRIGGER_MESSAGE)[1];
